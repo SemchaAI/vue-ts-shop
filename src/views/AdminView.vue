59e694a42c6ada11e9b8ccfe9b91192c724b7b5e
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-02-23 17:42:11 +0200

    +AdminPage

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,3 +1,52 @@
+<script setup lang="ts">
+import { useModal } from 'vue-final-modal'
+import ModalType from '@/components/modals/ModalType.vue'
+import ModalBrand from '@/components/modals/ModalBrand.vue'
+import ModalProduct from '@/components/modals/ModalProduct.vue'
+
+const ModalTypeInstance = useModal({
+  component: ModalType,
+  attrs: {
+    onConfirm() {
+      ModalTypeInstance.close()
+    }
+  }
+})
+const ModalBrandInstance = useModal({
+  component: ModalBrand,
+  attrs: {
+    onConfirm() {
+      ModalBrandInstance.close()
+    }
+  }
+})
+const ModalProductInstance = useModal({
+  component: ModalProduct,
+  attrs: {
+    onConfirm() {
+      ModalProductInstance.close()
+    }
+  }
+})
+</script>
+
 <template>
-  <div class="wrapper">Admin</div>
+  <section class="admin">
+    <div class="wrapper">
+      <div class="adminContainer">
+        <h1>Admin Panel</h1>
+        <div class="adminContent">
+          <div class="adminBlock">
+            <button @click="ModalTypeInstance.open">Add type</button>
+          </div>
+          <div class="adminBlock">
+            <button @click="ModalBrandInstance.open">Add Brand</button>
+          </div>
+          <div class="adminBlock">
+            <button @click="ModalProductInstance.open">Add Product</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
