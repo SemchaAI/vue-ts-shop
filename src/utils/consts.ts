9d60d32c026108f345c8958b0d1012f6e7759ef6
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-02-23 17:43:20 +0200

    +Consts URLS
    const urls for updated api

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -2,3 +2,7 @@ export const USER_ROUTE_LOGIN = '/user/login'
 export const USER_ROUTE_REGISTRATION = '/user/registration'
 export const USER_ROUTE_LOGOUT = '/user/logout'
 export const USER_ROUTE_USERS = '/user/users'
+
+export const PRODUCT_ROUTE = '/product'
+
+export const PRODUCT_ROUTE_TYPE = '/type'
