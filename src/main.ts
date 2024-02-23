8581450232efe5cabbaf99a0e2c9f6ee0e0f148b
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-02-23 17:37:36 +0200

    +plugin
    modal plugin connection with css

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import './assets/main.css'
+import 'vue-final-modal/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { VueQueryPlugin } from '@tanstack/vue-query'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+import { createVfm } from 'vue-final-modal'
 
 import App from './App.vue'
 import router from './router'
@@ -13,5 +15,6 @@ app.use(createPinia())
 app.use(router)
 app.use(VueQueryPlugin)
 app.use(autoAnimatePlugin)
+app.use(createVfm())
 
 app.mount('#app')
