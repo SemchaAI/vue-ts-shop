fc904b7e192661b6eb4c98b11202a1627d440779
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-02-26 16:26:18 +0200

    +upd
    it is temp file, was created for a mistake, i will delete and will move all data from here, sometimes

--- src/components/productList/product.interface.ts
+++ src/components/productList/product.interface.ts
@@ -1,14 +1,19 @@
+import type { IInfo, IProduct } from '@/models/IProduct'
+
 export interface IProductList {
   products: IProduct[]
   total: number
   page: number
 }
+export type OmittedProductList = Omit<IProductList, 'total' | 'page'>
 
-export interface IProduct {
-  _id: number
-  title: string
-  description: string
-  imgLink: string
-  cnt: number
-  price: number
-}
+// export interface IProduct {
+//   _id: number
+//   title: string
+//   description: string
+//   img: string
+//   cnt: number
+//   price: number
+//   typeId: string
+//   info: IInfo[]
+// }
