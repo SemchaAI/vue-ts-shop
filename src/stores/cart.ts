4adf4897dbc1c7511bed5936ac4cc7b06871819b
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-02-27 18:08:59 +0200

    +cart store

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -1,12 +1,33 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-import type { IProduct } from '@/components/productList/product.interface'
+import type { IProduct } from '@/models/IProduct'
+import { CartService } from '@/services/cart.service'
+import { AxiosError } from 'axios'
 
 export const useCartStore = defineStore('cart', () => {
   const state = ref({
     items: [] as IProduct[],
     total: 0
   })
+  // const userId = computed(() => user.state.user.id)
+  const items = computed(() => state.value.items)
 
-  return { state }
+  function setCartProducts(arr) {
+    state.value.items = arr
+  }
+
+  async function getCartProducts(userId: string) {
+    try {
+      const response = await CartService.getAll(userId)
+      return response
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        console.log(e.response?.data?.message)
+      } else {
+        console.log('WTF WITH THIS ERROR?', e)
+      }
+    }
+  }
+
+  return { state, items, getCartProducts, setCartProducts }
 })
