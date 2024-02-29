8eb31dc8c5758c2b6b0974574cea3affe776af89
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-02-29 17:27:07 +0200

    +footer

---
+++ src/components/footer/FooterComponent.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const year = new Date().getFullYear()
+</script>
+<template>
+  <footer class="footer">
+    <div class="wrapper">
+      <div class="footerContainer">
+        <p class="footerText">© {{ year }}. All rights reserved.</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+.footer {
+  margin-top: 40px;
+  background-color: var(--background-soft);
+}
+.footerContainer {
+  height: 80px;
+  display: flex;
+  align-items: center;
+}
+.footerText {
+  width: 100%;
+
+  @include body2Typo;
+  text-align: end;
+}
+</style>
