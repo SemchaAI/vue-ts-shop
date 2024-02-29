f3223580367ab58df8ac934ccc7b517347b4cbe4
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-02-29 17:26:14 +0200

    +swiper +footer
    swiper init

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,17 @@
 import { RouterView } from 'vue-router'
 import { ModalsContainer } from 'vue-final-modal'
 import GlobalHeader from './components/header/GlobalHeader.vue'
+import FooterComponent from './components/footer/FooterComponent.vue'
+
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle'
+
+register()
 </script>
 
 <template>
   <GlobalHeader />
   <RouterView />
   <ModalsContainer />
+  <FooterComponent />
 </template>
