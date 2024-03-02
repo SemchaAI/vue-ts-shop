e81615a1ae8fc24c0cfcb094bd610f446866e012
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-03-02 22:09:30 +0200

    +skeleton

--- src/components/pagination/ProductsPagination.vue
+++ src/components/pagination/ProductsPagination.vue
@@ -3,9 +3,10 @@ import { storeToRefs } from 'pinia'
 import { useProductsStore } from '@/stores/product'
 import ChevronLeftVue from '../icons/ChevronLeft.vue'
 import ChevronRight from '../icons/ChevronRight.vue'
+import { EStatus } from '@/models/IProduct'
 
 const productsStore = useProductsStore()
-const { page, total } = storeToRefs(productsStore)
+const { page, total, state } = storeToRefs(productsStore)
 const nextPage = () => {
   if (total.value > page.value) {
     productsStore.setPage(Number(page.value) + 1)
@@ -21,9 +22,10 @@ const toPage = (page: number) => {
     productsStore.setPage(page)
   }
 }
+console.log(state.value.status === EStatus.SUCCESS)
 </script>
 <template>
-  <div class="paginationContainer">
+  <div v-if="state.status === EStatus.SUCCESS" class="paginationContainer">
     <button class="paginationListBtn" @click.prevent="prevPage">
       <ChevronLeftVue />
     </button>
@@ -42,6 +44,7 @@ const toPage = (page: number) => {
       <ChevronRight />
     </button>
   </div>
+  <div v-else class="fakePagination skeleton">No products</div>
 </template>
 
 <style scoped lang="scss">
@@ -72,5 +75,29 @@ const toPage = (page: number) => {
 }
 .active {
   @include colorMix(secondary);
+}
+
+.skeleton {
+  width: 100%;
+  background-color: var(--on-white-skeleton);
+  color: var(--on-white-skeleton);
+  cursor: none;
+  border-radius: 20px;
+  background: linear-gradient(
+      100deg,
+      rgba(255, 255, 255, 0) 40%,
+      rgba(255, 255, 255, 0.5) 50%,
+      rgba(255, 255, 255, 0) 60%
+    )
+    var(--on-white-skeleton);
+  background-size: 200% 100%;
+  background-position-x: 180%;
+  animation: 1s loading ease-in-out infinite;
+}
+.fakePagination {
+  padding: 5px 20px;
+  width: 300px;
+  height: 35px;
+  margin: 0 auto;
 }
 </style>
