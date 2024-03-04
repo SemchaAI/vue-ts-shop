3dbe7b8a3247d1adfce7468c28d5b699b0d0e6e8
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-03-04 18:10:05 +0200

    +vue select css

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import 'vue-select/dist/vue-select.css'
+
 import './assets/main.scss'
 import 'vue-final-modal/style.css'
 import 'animate.css'
@@ -7,11 +9,13 @@ import { createPinia } from 'pinia'
 import { VueQueryPlugin } from '@tanstack/vue-query'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import { createVfm } from 'vue-final-modal'
+// import { VueSelect } from 'vue-select'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+// app.component('v-select', VueSelect)
 app.use(createPinia())
 app.use(router)
 app.use(VueQueryPlugin)
