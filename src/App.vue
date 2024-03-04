4f09ef0de38e62d8153947e0019dbc6214434ec1
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-03-04 18:04:58 +0200

    +global Error component

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,16 @@ import { RouterView } from 'vue-router'
 import { ModalsContainer } from 'vue-final-modal'
 import GlobalHeader from './components/header/GlobalHeader.vue'
 import FooterComponent from './components/footer/FooterComponent.vue'
+import ErrorComponent from './components/error/ErrorComponent.vue'
 
 // import function to register Swiper custom elements
 import { register } from 'swiper/element/bundle'
+
+import { useErrorStore } from './stores/erorrs'
+import { storeToRefs } from 'pinia'
+
+const errorStore = useErrorStore()
+const { state, isCritical, isError } = storeToRefs(errorStore)
 
 register()
 </script>
@@ -15,6 +22,14 @@ register()
   <main class="main">
     <RouterView />
   </main>
-  <ModalsContainer />
   <FooterComponent />
+
+  <ModalsContainer />
+  <ErrorComponent v-if="isError" :errors="state.errors" />
 </template>
+
+<style scoped lang="scss">
+.main {
+  min-height: 720px;
+}
+</style>
