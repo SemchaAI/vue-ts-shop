5f6039a7bf56559ae761bc2d01b773e99475b6d5
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-03-06 15:47:52 +0200

    +thumbnails

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -4,7 +4,7 @@ import InnerImageZoom from 'vue-inner-image-zoom'
 import { ProductService } from '@/services/product.service'
 
 import { useRoute } from 'vue-router'
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useProductsStore } from '@/stores/product'
 import type { IProduct } from '@/models/IProduct'
 import SearchIcon from '@/components/icons/SearchIcon.vue'
@@ -18,11 +18,26 @@ const isLoading = ref(false)
 const product = ref({
   data: {} as IProduct
 })
+const gallery = computed(() => {
+  if (product.value.data.thumbnails) {
+    return [product.value.data.img, ...product.value.data.thumbnails.img]
+  } else {
+    return [product.value.data.img]
+  }
+})
 
-// const { data: product, isSuccess } = useQuery({
-//   queryKey: ['getProduct'],
-//   queryFn: () => ProductService.getOne(id)
-// })
+// const urlHandler = (url: string) => {
+//   const thumbnails = product.value.data.thumbnails.img
+//   const mainImg = product.value.data.img
+//   product.value.data.thumbnails.img = thumbnails.filter((item: string) => {
+//     if (item === url) {
+//       item = mainImg
+//     }
+//     return item
+//   })
+//   product.value.data.img = url
+// }
+
 onMounted(async () => {
   product.value = await ProductService.getOne(id)
   // const tmp = await productsStore.getTypes()
@@ -35,25 +50,78 @@ onMounted(async () => {
     <div class="wrapper">
       <div v-if="isLoading" class="productContainer">
         <div class="productMain">
-          <!-- <img
-            width="500px"
-            height="500px"
-            class="productImg"
-            :src="url + '/' + product.data.img"
-            :alt="product.data.title"
-          /> -->
-          <div class="productImgContainer">
-            <inner-image-zoom
-              :width="450"
-              :height="450"
-              class="productImg"
-              :src="url + '/' + product.data.img"
-              :alt="product.data.title"
-              :hideHint="true"
-            />
-            <SearchIcon width="24" height="24" class="productImgHintIcon" />
-          </div>
+          <div class="productMedia">
+            <!-- <swiper-container class="bannerSwiper">
+              <swiper-slide v-for="thumbnail in product?.data.thumbnails.img" :key="thumbnail">
+                <inner-image-zoom
+                  :width="150"
+                  :height="150"
+                  :src="url + '/' + thumbnail"
+                  :alt="product.data.title"
+                  :hideHint="true"
+                />
+              </swiper-slide>
+            </swiper-container> -->
+            <swiper-container
+              class="productSwiper2"
+              space-between="0"
+              slides-per-view="3"
+              free-mode="true"
+              watch-slides-progress="true"
+              direction="vertical"
+            >
+              <swiper-slide
+                class="productSwiperSlide"
+                v-for="thumbnail in gallery"
+                :key="thumbnail"
+              >
+                <img
+                  class="productSwiperSlideImg2"
+                  width="150"
+                  height="150"
+                  :src="url + '/' + thumbnail"
+                  :alt="product.data.title"
+                />
+              </swiper-slide>
+            </swiper-container>
+            <swiper-container
+              class="productSwiper"
+              thumbs-swiper=".productSwiper2"
+              navigation="true"
+            >
+              <swiper-slide v-for="thumbnail in gallery" :key="thumbnail">
+                <div class="productImgContainer">
+                  <inner-image-zoom
+                    :width="450"
+                    :height="450"
+                    class="productImg"
+                    :src="url + '/' + thumbnail"
+                    :alt="product.data.title"
+                    :hideHint="true"
+                  />
+                  <SearchIcon width="24" height="24" class="productImgHintIcon" />
+                </div>
+                <!-- <img
+                  width="450px"
+                  height="450px"
+                  :src="url + '/' + thumbnail"
+                  :alt="product.data.title"
+                /> -->
+              </swiper-slide>
+            </swiper-container>
 
+            <!-- <div class="productImgContainer">
+              <inner-image-zoom
+                :width="450"
+                :height="450"
+                class="productImg"
+                :src="url + '/' + product.data.img"
+                :alt="product.data.title"
+                :hideHint="true"
+              />
+              <SearchIcon width="24" height="24" class="productImgHintIcon" />
+            </div> -->
+          </div>
           <div class="productColumn">
             <h1 class="productTitle">{{ product?.data.title }}</h1>
             <p class="productDescription">{{ product?.data.description }}</p>
@@ -68,7 +136,7 @@ onMounted(async () => {
             </div>
           </div>
         </div>
-        <div class="productSecond" v-if="product?.data.info">
+        <div class="productSecond" v-if="product.data.info.length > 0">
           <h2 class="productCharTitle">Характеристики</h2>
           <div class="productCharacteristics">
             <div
@@ -81,6 +149,7 @@ onMounted(async () => {
             </div>
           </div>
         </div>
+        <div v-else class="productCharTitle">Характеристик нет</div>
       </div>
       <div v-else>Loading...</div>
     </div>
@@ -170,5 +239,39 @@ onMounted(async () => {
   color: var(--primary);
 }
 .productInfoText {
+}
+
+//Product
+.productMedia {
+  display: flex;
+  width: 620px;
+  gap: 20px;
+  background-color: var(--background);
+}
+.productSwiper {
+  margin: 0;
+  max-width: 450px;
+  position: relative;
+  border-radius: 8px;
+
+  background-color: var(--tertiary);
+}
+
+.productSwiper2 {
+  margin: 0;
+  max-width: 150px;
+  max-height: 450px;
+  position: relative;
+
+  border-radius: 8px;
+
+  background-color: var(--tertiary);
+}
+.productSwiperSlide2 {
+  max-width: 150px;
+  max-height: 150px;
+}
+.productSwiperSlideImg2 {
+  border-radius: 8px;
 }
 </style>
